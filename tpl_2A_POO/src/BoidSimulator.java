0a35ee4448cf6910884bf9b623408a9457b4307f
Amaury <amaury.boin@gmail.com>
2015-11-14 15:42:26 +0100

    BS 1

--- tpl_2A_POO/src/BoidSimulator.java
+++ tpl_2A_POO/src/BoidSimulator.java
@@ -5,6 +5,7 @@
  */
 package src;
 import java.util.Stack; 
+import java.awt.Point;
 
 /**
  *
@@ -26,7 +27,7 @@ public class BoidSimulator {
         
         for (int i = 0; i < h/r; i++) {
             for (int j = 0; j < l/r; j++) {
-                hachage[i][j] = new Stack ();
+                hachage[i][j] = new Stack (); //Initialise le tableau de jeu avec des piles de Boids
             }
         }
     }
@@ -35,12 +36,32 @@ public class BoidSimulator {
         return (x+y)%y ;
     }
     
-    private int[] access (int [] v){ //v doit être un couple (x,y)
-        v[0] = mod (v[0],hauteur/rayon);
-        v[1] = mod (v[1],longueur/rayon);
-        return v;
+    private void access (Point v){ //v doit être un couple (x,y)
+        v.x = mod (v.x,hauteur/rayon);
+        v.y = mod (v.y,longueur/rayon);
     }
     
+    private Point caseCorrespondante (Boids b){ //Calcule la position d'un boids dans le plateau
+        Point p = new Point ();
+        p.setLocation( b.getlocalisation().getX() / b.getRayon (), b.getlocalisation().getY() / b.getRayon ());
+        return p;
+    }
+    
+    private Stack voisinsPotentiels (Boids b){ //retourne la pile des boids dans les 9 cases entourant un boid
+        Stack pile = new Stack ();
+        Point caseDuBoid = caseCorrespondante (b);
+        Point acc = new Point ();
+        
+        for (int i = -1; i<2; i++){
+            for (int j = -1; j<2; j++){
+                acc.setLocation(caseDuBoid.getX() + i,caseDuBoid.getY() + j);
+                access (acc);
+                pile.addAll(hachage [acc.x][acc.y]);
+            }
+        }
+        
+        return pile; 
+    }
     
     
     
