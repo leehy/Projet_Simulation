03d0aa3df2e6ce7c6786133b8bed0a0cbee5d9cf
sacha <sacha@sacha-PC>
2015-11-15 20:09:08 +0100

    le poisson aime la lumiere

--- tpl_2A_POO/src/Poisson.java
+++ tpl_2A_POO/src/Poisson.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package src;
+
 import gui.*;
 import java.awt.Color;
 import java.awt.Point;
@@ -14,14 +15,15 @@ import java.util.*;
  * @author sacha
  */
 public class Poisson extends Boids {
-int tailleTete;
-int tailleCorpsLongueur;
-int tailleCorpsHauteur;
-Color couleurTete;
-Color couleurCorps;
+
+    int tailleTete;
+    int tailleCorpsLongueur;
+    int tailleCorpsHauteur;
+    Color couleurTete;
+    Color couleurCorps;
 
     public Poisson(int x, int y, int Vx, int Vy, int rayonAction, int rayonSecurite, int tailleHauteur, int tailleLongueur) {
-        super( x, y, Vx, Vy, rayonAction,rayonSecurite, tailleHauteur,  tailleLongueur);
+        super(x, y, Vx, Vy, rayonAction, rayonSecurite, tailleHauteur, tailleLongueur);
         this.setEtat(1); //un poisson sera dans l'état 1
         this.tailleCorpsLongueur = 30;
         this.tailleTete = 10;
@@ -29,46 +31,49 @@ Color couleurCorps;
         this.couleurTete = Color.pink;
         this.couleurCorps = Color.cyan;
     }
-    
+
     @Override
     public void afficheBoid(GUISimulator gui) {
-        GraphicalElement corps = new Oval((int)this.getlocalisation().getX(), 
-                (int)this.getlocalisation().getY(),
-                this.couleurCorps, Color.GRAY, 
+        GraphicalElement corps = new Oval((int) this.getlocalisation().getX(),
+                (int) this.getlocalisation().getY(),
+                this.couleurCorps, Color.GRAY,
                 this.tailleCorpsLongueur, this.tailleCorpsHauteur);
-        GraphicalElement tete = new Oval((int)this.getlocalisation().getX() + tailleCorpsLongueur/2, 
-                (int)this.getlocalisation().getY(),
-                this.couleurTete, Color.GRAY, 
+        GraphicalElement tete = new Oval((int) this.getlocalisation().getX() + tailleCorpsLongueur / 2,
+                (int) this.getlocalisation().getY(),
+                this.couleurTete, Color.GRAY,
                 this.tailleTete);
         gui.addGraphicalElement(corps);
         gui.addGraphicalElement(tete);
     }
-    
 
-@Override
+    @Override
 //on override le centre de masse de sorte a ce que si le poisson a dans ses voisins une lumiere
 //il sera vraiment très attiré par la lumière, beaucoup plus que par ses amis poissons 
 //(il est attiré par les poisssons de l'ordre de 1% et par la lumière de l'ordre de 100%)
-public Point centreDeMasse() {
+    public Point centreDeMasse() {
         int index = 0;
         Point centreMasse = new Point();
         int taille = this.voisins.size();
-        while (index != taille) {
+        while (index < taille) {
             //si c'est un poisson le poids reste le meme
-            if(this.voisins.get(index).getEtat() == 1)
-            {
-            centreMasse.setLocation(centreMasse.getX() + this.voisins.get(index).getlocalisation().getX() / taille,
-                    centreMasse.getY() + this.voisins.get(index).getlocalisation().getY() / taille);
+            if (this.voisins.get(index).getEtat() == 1) {
+                centreMasse.setLocation(centreMasse.getX() + this.voisins.get(index).getlocalisation().getX() / taille,
+                        centreMasse.getY() + this.voisins.get(index).getlocalisation().getY() / taille);
             }
             //si c'est une lumiere le poids est bien plus important (2*nombreDeVoisins fois plus important)
-            else if(this.voisins.get(index).getEtat() == 2)
-            {
-             centreMasse.setLocation(centreMasse.getX() + this.voisins.get(index).getlocalisation().getX() ,
-                    centreMasse.getY() + this.voisins.get(index).getlocalisation().getY() );
+            /*else if(this.voisins.get(index).getEtat() == 2)
+             {
+             centreMasse.setLocation(centreMasse.getX() + this.voisins.get(index).getlocalisation().getX()*10 ,
+             centreMasse.getY() + this.voisins.get(index).getlocalisation().getY()*10 );
+             }*/
+            if (this.voisins.get(index).getEtat() == 2) {
+
+                centreMasse.setLocation(this.voisins.get(index).getlocalisation());
+                index++;
             }
             index++;
+
         }
         return centreMasse;
     }
 }
-
